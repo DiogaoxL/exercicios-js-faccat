ba4772f57de11bc34935488a709501b3e5ba48da
Diogo Duarte <85971348+DiogaoxL@users.noreply.github.com>
2023-11-17 22:01:40 -0300

    Update ex 24.js

--- ex 24/ex 24.js
+++ ex 24/ex 24.js
@@ -4,16 +4,17 @@ ele recebe uma comissão de 3% sobre o total das vendas até R$ 1.500,00 mais 5%
 ultrapassar este valor, calcular e escrever o seu salário total. 
 */
 
-let salario_fixo = 1200
-let valor_vendas = 8000
+let salario_fixo = 1000
+let valor_vendas = 2000
 let comissao = 0
 
 if (valor_vendas <= 1500) {
   comissao = (valor_vendas * 3) / 100
   document.write('seu novo salario é : ' + (comissao + salario_fixo))
 } else {
-  if (valor_vendas > 1500) {
-    comissao = (valor_vendas * 5) / 100
+    comissao = ((1500 * 3) / 100) + (((valor_vendas - 1500) * 5) / 100)
     document.write('seu novo salario é : '+ (comissao + salario_fixo))
-  }
 }
+
+// 45 + 25 = 70
+// 1000 + 70 = 1070
